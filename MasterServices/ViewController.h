1eef531f45121d2ee384d6ca45ab2c98b21cc961
Ivan Novikov <evans.novikov@gmail.com>
2015-10-31 05:02:06 +0300

    Initial Commit

---
+++ MasterServices/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasterServices
+//
+//  Created by Ivan Novikov on 31.10.15.
+//  Copyright © 2015 inov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
